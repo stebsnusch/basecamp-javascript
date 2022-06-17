cebd1056db978e31bd32d431795bede434b91903
gwfb2 <guilhermewilson63@gmail.com>
2022-06-17 16:33:03 -0400

    mostra -1 se o array estiver vazio

--- variaveis-e-tipos/substituiPares2.js
+++ variaveis-e-tipos/substituiPares2.js
@@ -1,13 +1,15 @@
 function substituiPares (vetor){
     if(!vetor) return;
+    if(!vetor.length) console.log(-1);
     
     for (let i = 0; i < vetor.length; i++) {
         if (vetor[i] % 2 == 0) {
             vetor[i] = 0;
         }
+        console.log(vetor);
     }
 
-    console.log(vetor);
+    
 }
 
-substituiPares([0]);
+substituiPares([]);
