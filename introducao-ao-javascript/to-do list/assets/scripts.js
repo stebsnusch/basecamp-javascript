3ab46a3663b6975c8c4d7a8d133aac7eba7b3d07
guilherme <105934222+gwfb2@users.noreply.github.com>
2022-08-01 14:19:14 -0400

    adiciona função de excluir task

--- introducao-ao-javascript/to-do list/assets/scripts.js
+++ introducao-ao-javascript/to-do list/assets/scripts.js
@@ -1,29 +1,49 @@
-const form = document.getElementById('task-form');
-const taskList = document.getElementById('tasks');
+function doThings (e) {
+    e.preventDefault();
 
-form.onsubmit = function (e) {
-	e.preventDefault();
-	const inputField = document.getElementById('task-input');
-	addTask(inputField.value);
-	form.reset();
-};
+    if(!input.value) return alert('Tarefa vazia!');
 
-function addTask(description) {
-	const taskContainer = document.createElement('div');
-	const newTask = document.createElement('input');
-	const taskLabel = document.createElement('label');
-	const taskDescriptionNode = document.createTextNode(description);
+    // creates elements
+    const taskItem = document.createElement('div');
+    const task = document.createElement('div');
+    const checkbox = document.createElement('input');
+    const label = document.createElement('label');
+    const removeBtn = document.createElement('button');
+    
+    // structures DOM tree 
+    tasks.appendChild(taskItem);
+    taskItem.appendChild(task);
+    taskItem.appendChild(removeBtn);
+    task.appendChild(checkbox);
+    task.appendChild(label);
 
-	newTask.setAttribute('type', 'checkbox');
-	newTask.setAttribute('name', description);
-	newTask.setAttribute('id', description);
+    // sets attributes to elements
+    taskItem.setAttribute('class', 'task-item');
+    taskItem.setAttribute('name', input.value);
+    taskItem.setAttribute('id', input.value);
+    
+    checkbox.setAttribute('type','checkbox');
+    checkbox.setAttribute('for', input.value);
+    
+    // captures input content and give it to task's label
+    label.innerHTML = input.value;
 
-	taskLabel.setAttribute('for', description);
-	taskLabel.appendChild(taskDescriptionNode);
+    // remove button receives some text content
+    removeBtn.innerHTML = 'Remover';
 
-	taskContainer.classList.add('task-item');
-	taskContainer.appendChild(newTask);
-	taskContainer.appendChild(taskLabel);
-
-	taskList.appendChild(taskContainer);
+    // after the task has been added, the input is cleared
+    input.value = ''
+    
+    // defines an event listener for the event of click on the remove button
+    removeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        taskItem.remove()
+    });
 }
+
+const input = document.getElementById('task-input');
+const addBtn = document.getElementById('addBtn');
+const tasks = document.getElementById('tasks');
+
+addBtn.addEventListener('click', doThings);
